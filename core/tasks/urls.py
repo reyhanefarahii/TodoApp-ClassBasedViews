7c062efe3f613b9c602fefbd96475b7591dbcf19
reyhanefarahii <missreyhanefarahi@gmail.com>
2026-09-21 17:21:22 +0330

    Add CRUD Operations for Tasks and Status Update

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import TaskView,TaskUpdate,TaskDelete,TaskCreate,TaskStatus
 urlpatterns = [
-    path('', indexview ),
+    path('', TaskView.as_view(), name='task-list'),
+    path('<int:pk>/update', TaskUpdate.as_view(), name='task-update'),
+    path('<int:pk>/delete', TaskDelete.as_view(), name='task-delete'),
+    path('create/', TaskCreate.as_view(), name='task-create'),
+    path('<int:pk>/status/', TaskStatus.as_view(), name='task-status'),
 ]
--- core/tasks/views.py
+++ core/tasks/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView,UpdateView,DeleteView,CreateView
+from .models import Task
 
 # Create your views here.
-def indexview(request):
-    return render(request,'index.html')
+class TaskView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    ordering = ['-created_at']
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title']
+    template_name = 'tasks/task_form.html'
+    success_url = "/"
+
+class TaskDelete(DeleteView):
+    model = Task
+    success_url = "/"
+    # template_name = "geeks/geeksmodel_confirm_delete.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['title']
+    success_url = '/'
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        return redirect('task-list')
+
+class TaskStatus(UpdateView):
+    model = Task
+    fields = ['status']
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.status = not task.status
+        task.save()
+        return redirect('task-list')
