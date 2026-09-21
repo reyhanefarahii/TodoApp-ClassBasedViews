7034d96204ab49fda702bdabab716753321ee6ea
reyhanefarahii <missreyhanefarahi@gmail.com>
2026-09-21 12:42:58 +0330

    Add Task model and configure Django admin

--- core/core/urls.py
+++ core/core/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),
+    path('', include('tasks.urls')),
 ]
--- core/tasks/admin.py
+++ core/tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    fields = ('user', 'title', 'status', 'created_at', 'updated_at')
+    list_display=('user', 'title', 'status', 'created_at', 'updated_at')
+    list_filter = ('status','user')
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Task,TaskAdmin)
